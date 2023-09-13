3af72ac64069c1e7e6a7088d95ffee0d11669250
Jerry <jerry11368@gmail.com>
2023-09-13 16:38:25 -0400

    more contacts stuff

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2fe8ba3918b5953c"; }
+/******/ 		__webpack_require__.h = function() { return "fd183ec3fbbbc90a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
